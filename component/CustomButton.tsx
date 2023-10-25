0e7499403a89416d2620e41efce60792a94d6224
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-25 22:51:34 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

---
+++ component/CustomButton.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const CustomButton = () => {
+  return (
+    <button
+      disabled={false}
+      type={"button"}
+      className={`custom-btn`}
+      onClick={() => {}}
+    ></button>
+  );
+};
+
+export default CustomButton;
