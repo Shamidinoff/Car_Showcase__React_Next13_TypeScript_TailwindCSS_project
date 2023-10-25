be74ba0d521c1ebabd5444ef97efb46118b5e64d
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-25 22:33:56 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import { Hero } from "@/component";
 
 export default function Home() {
-  return <main className="overflow-hidden"></main>;
+  return (
+    <main className="overflow-hidden">
+      <Hero />
+    </main>
+  );
 }
