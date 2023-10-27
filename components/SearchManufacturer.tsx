692b9d4f00aa1034c285fc05e3421ba981edcb8e
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-27 21:52:45 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { Combobox, Transition } from "@headlessui/react";
+import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
 import Image from "next/image";
 
@@ -9,6 +10,16 @@ const SearchManufacturer = ({
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState("");
+
+  const filteredManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLocaleLowerCase().replace(/\s+/g, ""))
+        );
 
   return (
     <div className="search-manufacturer">
@@ -30,6 +41,39 @@ const SearchManufacturer = ({
             displayValue={(manufacturer: string) => manufacturer}
             onChange={(e) => setQuery(e.target.value)}
           />
+
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filteredManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                  value={query}
+                  className="search-manufacturer__option"
+                >
+                  Create '{query}'
+                </Combobox.Option>
+              ) : (
+                filteredManufacturers.map((item) => (
+                  <Combobox.Option
+                    key={item}
+                    className={({ active }) => `
+                    relative search-manufacturer__option ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }
+                    `}
+                    value={item}
+                  >
+                    {item}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
         </div>
       </Combobox>
     </div>
