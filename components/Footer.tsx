1df38c6ce54e8a1f7931e491e8f77a3b07de9da9
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-27 20:15:18 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -38,6 +38,19 @@ const Footer = () => {
           ))}
         </div>
       </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All Rights Reserved</p>
+
+        <div className="footer__copyright-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 };
