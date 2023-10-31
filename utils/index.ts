44dbf3b2ec133529f58bd05c430592a5dbcbcb5a
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-10-31 20:55:20 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- utils/index.ts
+++ utils/index.ts
@@ -1,20 +1,3 @@
-const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "c0519d6039msha7fb98572389e6dp1ccdb2jsnf69372388e2e",
-    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
-
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
-}
-
 export async function fetchCars() {
   const headers = {
     "X-RapidAPI-Key": "c0519d6039msha7fb98572389e6dp1ccdb2jsnf69372388e2e",
@@ -29,4 +12,6 @@ export async function fetchCars() {
   );
 
   const result = await response.json();
+
+  return result;
 }
