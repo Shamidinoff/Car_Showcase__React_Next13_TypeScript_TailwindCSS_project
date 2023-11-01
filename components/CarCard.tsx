8449a8e672574e5bc2baebf9863f08f6fdcfdf29
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-01 20:20:50 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -37,6 +37,39 @@ const CarCard = ({ car }: CarCardProps) => {
           className="object-contain"
         />
       </div>
+
+      <div className="relative flex w-full mt-2">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image src="/tire.svg" width={20} height={20} alt="tire" />
+            <p className="text-[14px]">{drive.toUpperCase()}</p>
+          </div>
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <Image
+              src="/steering-wheel.svg"
+              width={20}
+              height={20}
+              alt="steering wheel"
+            />
+            <p className="text-[14px]">
+              {transmission === "a" ? "Automatic" : "Manual"}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
