4b77b15954afff3810200132b88320b6b2a0f722
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-02 21:09:46 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -57,7 +57,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   <div className="flex-1 flex flex-col gap-3">
                     <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
                       <Image
-                        src={generateCarImageUrl(car, "angle")}
+                        src={generateCarImageUrl(car)}
                         alt="car model"
                         fill
                         priority
@@ -68,7 +68,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <div className="flex gap-3">
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "29")}
                           alt="car model"
                           fill
                           priority
@@ -78,7 +78,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
 
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "33")}
                           alt="car model"
                           fill
                           priority
@@ -88,7 +88,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
 
                       <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
                         <Image
-                          src={generateCarImageUrl(car, "angle")}
+                          src={generateCarImageUrl(car, "13")}
                           alt="car model"
                           fill
                           priority
