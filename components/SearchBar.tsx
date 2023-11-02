a0081ee4a96068d0843a8cbdb67a457accdafbd8
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-02 21:34:19 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { useState } from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import SearchManufacturer from "./SearchManufacturer";
 import Image from "next/image";
 
@@ -16,9 +17,44 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 );
 
 const SearchBar = () => {
-  const handleSearch = () => {};
   const [manufacturer, setManufacturer] = useState("");
   const [model, setModel] = useState("");
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar");
+    }
+
+    updateSearchParams(
+      model.toLocaleLowerCase(),
+      manufacturer.toLocaleLowerCase()
+    );
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+
+    if (model) {
+      searchParams.set("model", model);
+    } else {
+      searchParams.delete("model");
+    }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer);
+    } else {
+      searchParams.delete("manufacturer");
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`;
+
+    router.push(newPathname);
+  };
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
