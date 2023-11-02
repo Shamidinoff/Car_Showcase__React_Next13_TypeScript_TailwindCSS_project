08eec75cc9d12eeee5be6b5bbee44b78684edf6c
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-02 21:55:26 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- types/index.ts
+++ types/index.ts
@@ -30,3 +30,11 @@ export interface CarProps {
   transmission: string;
   year: number;
 }
+
+export interface FilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
