d57b508fff4e888616fbc103edd9947d5ff3dbc7
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-03 21:12:30 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -13,7 +13,28 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option>
+                  {({ selected }) => <span>{option.title}</span>}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
