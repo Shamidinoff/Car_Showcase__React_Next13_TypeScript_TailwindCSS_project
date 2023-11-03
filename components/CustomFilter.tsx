427382e048a06fb567c5302f492afe854af9390f
Daurenbek Shamidinov <120316396+Shamidinoff@users.noreply.github.com>
2023-11-03 21:44:32 +0600

    Car Showcase - Next JS 13, React, TypeScript, Tailwind CSS project

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -4,12 +4,27 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(options[0]);
+
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLocaleLowerCase());
+
+    router.push(newPathName);
+  };
+
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
@@ -29,8 +44,24 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
           >
             <Listbox.Options className="custom-filter__options">
               {options.map((option) => (
-                <Listbox.Option>
-                  {({ selected }) => <span>{option.title}</span>}
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <span
+                      className={`block truncate ${
+                        selected ? "font-medium" : "font-normal"
+                      }`}
+                    >
+                      {option.title}
+                    </span>
+                  )}
                 </Listbox.Option>
               ))}
             </Listbox.Options>
